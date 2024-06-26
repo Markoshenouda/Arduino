46fb44c6d066b830f0135866967e5ef099d7deec
marko shenouda <136264452+Markoshenouda@users.noreply.github.com>
2024-06-26 16:35:41 +0300

    Create Exercise_1.ino

---
+++ section 3#/Exercise_1.ino
@@ -0,0 +1,45 @@
+/*int led_13 = 13;
+int led_12 = 12;
+int led_11 = 11;
+int led_10 = 10;
+int led_9 = 9;
+int led_8 = 8;*/
+
+int led[6] = {8,9,10,11,12,13};
+
+void setup() {
+  // put your setup code here, to run once:
+  /*pinMode(led_13, OUTPUT);
+  pinMode(led_12, OUTPUT);
+  pinMode(led_11, OUTPUT);
+  pinMode(led_10, OUTPUT);
+  pinMode(led_9, OUTPUT);
+  pinMode(led_8, OUTPUT);*/
+
+  for(int i=0; i<6; i++){
+    pinMode(led[i], OUTPUT);
+  }
+  
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  //#1 example
+  /*
+  for(int i=0; i<6; i++){
+    digitalWrite(led[i], HIGH);
+    delay(200);
+    digitalWrite(led[i], LOW);
+  }*/
+  //#2 example
+  for(int i=0; i<6; i=i+2){
+    digitalWrite(led[i], HIGH);
+    digitalWrite(led[i+1], LOW);
+  }
+    delay(500);
+   for(int i=0; i<6; i=i+2){
+    digitalWrite(led[i+1], HIGH);
+    digitalWrite(led[i], LOW);
+
+    }
+  delay(500);
+}
